7e740ac85e32e5599dec0663ddfec478599a11d1
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-03-18 17:04:47 -0400

    core users

--- server/app/modules/core/tables/core_users.js
+++ server/app/modules/core/tables/core_users.js
@@ -1,6 +1,6 @@
 module.exports = function (knex) {
   knex.schema.withSchema('public').createTable('core_users', table => {
-      table.uuid('id').unique().defaultTo(knex.schema.raw("gen_random_uuid()"))
+      table.uuid('id').unique().defaultTo(knex.schema.raw("public.gen_random_uuid()"))
     }).then(response => {
       console.log('table created')
       console.log(response)
