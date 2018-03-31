c051dd3a1f515333a0495e3853703516c600c82a
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-03-31 17:20:23 -0400

    minor changes

--- server/app/api/index.js
+++ server/app/api/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 router.use(express.json()) // not ideal. Should be added just before each route, only one relevant ones, after auth
-router.use(require('./services').session)
+router.use(require('@services/session'))
 
-require('./modules').forEach(module => {
-  router.use(`/api`, require(`./modules/${module}/routes`))
+require('@app/modules').forEach(m => {
+  router.use(`/api`, require(`@routes/${m}`))
 })
 
 module.exports = router
--- server/app/app.js
+++ server/app/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app
-  .use(require('./api'))
+  .use(require('@api'))
   .use('/', require('express').static('app/public'))
 
 module.exports = app
