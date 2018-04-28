12e6be4944e400112c77c651de3b40fb9b402a81
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-04-28 18:21:42 -0400

    more prototyping

--- server/app/tools/resolveInfo.js
+++ server/app/tools/resolveInfo.js
@@ -20,4 +20,37 @@ module.exports = class resolverInfo {
   }
 }
 
-class exampleModel
+class resolverInfo {
+  constructor ({ model, info, action, session, ac }) {
+    this.eager = [expression, filter] // eager expression 
+    this.inputGraph = {} // filtered input graph
+
+    this.resolverInfo = {
+
+    }
+  }
+
+  filter (data) {
+    // return filtered data
+  }
+
+}
+
+cachedPermissions = {
+  'Resource': {
+    'action:ownership': permission
+  }
+}
+
+function permission (resource, action, ownership) {
+  action = `${action}:${ownership ? 'own' : 'any'}`
+  // verify if exists. if not create
+  return permissionCache[resource][action]
+}
+
+
+function hasOwnerShip (node, model) {
+  // return boolean
+}
+// function to detemine what permision object create permission object
+// send to cache
