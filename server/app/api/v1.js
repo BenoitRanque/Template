1208a0b28bcbb70cb70608abdcec47128884ae31
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-05-06 16:42:06 -0400

    added session and access control middleware

--- server/app/api/v1.js
+++ server/app/api/v1.js
@@ -1,4 +1,10 @@
+const AccessControl = require('@tools/accesControl')
+const Session = require('@tools/session')
+
 const router = require('express').Router()
+
+router.use(Session)
+router.use(AccessControl.middelware())
 
 const modules = [
   'core'
