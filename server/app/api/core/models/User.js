6e6b6b3f961c6559dfda1610f584917bbc01d3ad
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-05-06 16:45:05 -0400

    updated User model to use new resolver

--- server/app/api/core/models/User.js
+++ server/app/api/core/models/User.js
@@ -40,7 +40,7 @@ module.exports = class User extends BaseModel {
       }
     }
   }
-  static get filters() {
+  static get queryFilters() {
     return {
       id: {
         type: 'string',
@@ -61,3 +61,50 @@ module.exports = class User extends BaseModel {
     }
   }
 }
+
+module.exports = new Model({
+  tableName: 'core_users',
+  idColumn: 'user_id',
+  name: 'CoreUser',
+  description: 'A User of the core module',
+  resource: 'CoreUser',
+  schema: {
+    type: 'object',
+    required: ['username'],
+    properties: {
+      'user_id': { type: 'string' },
+      'username': { 
+        description: 'a unique user identifier',
+        type: 'string'
+      },
+      'password': { type: 'string' },
+      'description': { type: 'string' }
+    }
+  },
+  relations: () => {
+    const Role = require('./Role')
+    return {
+      'role': {
+        graph: {
+          insert: false,
+          upsert: false
+        },
+        relation: ManyToManyRelation,
+        modelClass: Role,
+        join: {
+          from: 'core_users.user_id',
+          through: {
+            from: 'core_user_roles.user_id',
+            to: 'core_user_roles.role_id'
+          },
+          to: 'core_roles.role_id'
+        }
+      }
+    }
+  },
+  filters: {
+    id: (query, user_id) => query.where({ user_id }),
+    username: (query, username) => query.where({ username }),
+    own: (query, user_id) => query.where({ user_id })
+  }
+})
