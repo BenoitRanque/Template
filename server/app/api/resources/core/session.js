2fe2b81e2450c230d5ec84e6981d25b8ed005169
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-05-06 17:33:03 -0400

    added first complete resource. Should now be able to log in and out

--- server/app/api/resources/core/session.js
+++ server/app/api/resources/core/session.js
@@ -1,8 +1,29 @@
 module.exports = {
-  async login (info, tools, data, params) {
+  async login ({ session, fields }, tools, { username, password }, params) {
+    const bcrypt = require()
+    const User = require('@models/core/User')
 
+    let user = await User.query().where({ username }).eager('role').first()	
+    if (!user) throw new Error(401)	
+
+    let auth = await bcrypt.compare(password, user.password)	
+    if (!auth) throw new Error(401)	
+
+    delete user.password	
+    
+    session.user = user	
+
+    let permission = permission(User.resource, 'read', 'own')
+
+    if (!permission.granted) return
+
+    let result = await User.query().belongsTo(user.user_id).eagerFromFields(fields)
+    
+    return permission.filter(results)
   },
-  async logout (info, tools, data, params) {
+  async logout ({ session }, { authenticate }, data, params) {
+    authenticate()
 
+    session.destroy()
   }
 }
