1f773ea049147f2f013cd6f46db4d5a12674ea97
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-05-06 16:35:07 -0400

    new resolver finished

--- server/app/tools/resolver.js
+++ server/app/tools/resolver.js
@@ -1,61 +1,46 @@
 class Resolver {
-  constructor (model, method, config) {
-    {
-      // bracket scope
-      // validate configuration and set defaults
-      if (!model) throw new Error(`model is a required parameter of the resolver`)
-      if (!method) throw new Error(`method is a required parameter of the resolver`)
-  
-      this.model = model
-      this.method = method
-      this.config = { 
-        authenticate: true,
-        authorize: true,
-        params: null,
-        resource: model.resource, 
-        ...config
-      }
-    }
-    // separate read requests from the rest.
+  constructor (method, params) {
+    this.method = method
+    this.params = params ? params : () => {} 
+
     return async (req, res, next) => {
       try {
-        let
-          { ac, session, body } = req,
-          context = { ac, session },
-          data = body,
-          permission = null,
-          action = HTTPmethodToAction(req.method),
-          ownership = req.query ? !!req.query.own : false,
-          actionownership = `${action}:${ ownership ? 'own' : 'any' }`,
-          resource = this.config.resource,
+        let { accessControl, session, body, query, method } = req
+        let { possession, filters, fields } = query
 
-        if (this.config.authorize) {
-          permission = await ac.authorize(session, this.model.resource,  actionownership)
+        let action = HTTPmethodToAction(method)
+        let actionpossession = `${action}:${  }`
+
+        let permission = null
+
+        if (this.config.authorize && resource) {
+          permission = await accessControl.authorize(session, resource, actionown)
         }
         else if (this.config.authenticate) {
-          await ac.authenticate(session)
+          await accessControl.authenticate(session)
+        }
+        let info = {
+          action: HTTPmethodToAction(method),
+          possession: possession ? possession : 'any',
+          session,
+          fields: fields ? decode(fields) : [],
+          filters: filters ? decode(filters) : {},
+        }
+        let tools = {
+          accessControl,
+          authenticate: () => accessControl.authenticate(session),
+          authorize: (resource, action, possession) => accessControl.authorize(session, resource, action || info.action, possession || info.possession),
+          permission: (resource, action, possession) => accessControl.permission(session, resource, action || info.action, possession || info.possession),
+          recursiveFilter: (model, data, action, possession) => recursiveFilter(model, accessControl, data, action || info.action, possession || info.possession),
+          encode,
+          decode
         }
 
-        let
-          graph = new QueryGraph(this.model, req.query ? req.query.graph : null, true).authorize(resource => ac.permission(session, resource, action)),
-          params = this.config.params ? this.config.params(req, res, next) : null,
-          info = {
-            permission,
-            action,
-            ownership,
-            actionownership,
-            resource,
-            graph,
-            params
-          }
-        
-        let results = await this.method(this.model, data, info, context)
-        
-        if (permission) {
-          results = permission.filter(results)
-        }
+        let params = this.params(req, res, next)
 
-        res.status(200).json({ data: results })
+        let output = await this.method(info, tools, body, params)
+
+        res.status(200).json(output)
       }
       catch (error) {
         console.log(error)
@@ -65,50 +50,12 @@ class Resolver {
   }
 }
 
-class QueryGraph {
-  constructor (model, graph, encoded) {
-    this.model = model
-    if (encoded) graph = this.decode(graph)
-    this.graph = graph || []
-  }
+function encode (data) {
+  return encodeURI(btoa(JSON.stringify(data)))
+}
 
-  encode () {
-    return encodeURI(btoa(JSON.stringify(this.graph)))
-  }
-
-  decode () {
-    return JSON.parse(atob(decodeURI(this.graph)))
-  }
-
-  authorize(permit) {
-    // validate this graph as authorized
-  }
-
-  typeOf(field) {
-    return ({}).toString.call(obj).match(/\s([a-z|A-Z]+)/)[1].toLowerCase()
-  }
-
-  root (graph) {
-    if (!graph) graph = this.graph
-    // return top level property field
-    switch (this.typeOf(graph)) {
-      case 'string': return graph
-      case 'array': return graph[0]
-      default: throw new Error(`Invalid root field for graph ${graph}`)
-    }
-  }
-  fields (graph) {
-    if (!graph) graph = this.graph
-    if (!Array.isArray(graph)) return []
-    if (graph.length > 1 && this.typeOf(graph[1]) === 'object') return graph.slice(2)
-    return graph.slice(1)
-  }
-  filters (graph) {
-    if (!graph) graph = this.graph
-    if (!Array.isArray(graph)) return {}
-    if (graph.length > 1 && this.typeOf(graph[1]) === 'object') return graph[1]
-    return {} 
-  }
+function decode (data) {
+  return JSON.parse(atob(decodeURI(data)))
 }
 
 function HTTPmethodToAction (method) {
@@ -121,39 +68,42 @@ function HTTPmethodToAction (method) {
   }
 }
 
-function filterOutput (data, model, permit) {
+function recursiveFilter (model, accessControl, data, action, possession) {
   const permissionCache = {}
-
-  return recursiveFilter(data, model)
-
-  function permit (resource) {
-    if (!permissionCache[resource]) permissionCache[resource] = this.permit(resource)
-    return permissionCache[resource]
+  let permission = (resource, action) => {
+    if (!permissionCache[resource]) permissionCache[resource] = {}
+    if (!permissionCache[resource][action]) permissionCache[resource][action] = accessControl.permission(session, resource, action, possession)
+    return permissionCache[resource][action]
   }
 
-  function recursiveFilter (data, model) {
-    let permission = permit(model.resource)
-    if (!permission.granted) return null
-    
+  function filter (model, data) {
+    let permission = permission(model.resource, action).filter(data)
+    if (!permission.granted) return
     data = permission.filter(data)
-
-    if (Array.isArray(data)) return recursiveFilterArray(data, model)
-    return recursiveFilterObject(data, model)
-  }
-
-  function recursiveFilterArray (data, model) {
-    return data.map(item => recursiveFilterObject(item, model))
+  
+    if (Array.isArray(data)) {
+      return filterArray(model, data)
+    }
+    else {
+      return filterObject(model, data)
+    }
   }
   
-  function recursiveFilterObject (data, model) {      
+  function filterArray (model, data) {
+    return data.map(item => filterObject(model, item))
+  }
+  
+  function filterObject (model, data) {
     let relations = model.getRelations()
-    Object.keys(data).forEach(fieldName => {
-      let relation = relations[fieldName]
-      if (relation) data[fieldName] = recursiveFilter(data[fieldName], relation.relatedModelClass)
+    Object.keys(data).forEach(propName => {
+      let relation = relations[propName]
+      if (!relation) return
+      data[propName] = filter(relation.relatedModelClass, data[propName])               
     })
-
     return data
   }
+
+  return filter(model, data)
 }
 
 module.exports = Resolver
