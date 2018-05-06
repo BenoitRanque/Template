0963eeaedf541eb1055d0d05845e3e41d7df7abd
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-05-06 16:40:02 -0400

    new base model function

--- server/app/tools/model.js
+++ server/app/tools/model.js
@@ -1,23 +1,42 @@
 const { Model, QueryBuilder } = require('objection')
+const buildEager = require('./buildEager')
 
 Model.knex(require('@db/knex'))
 
 class BaseQueryBuilder extends QueryBuilder {
-  eagerFromGraph(graph, model) {
-    // build eager expression
-    // apply first level filters
 
-    const [expression, filters] = buildEager(graph.fields(), graph, model)
+  belongsTo(user_id) {
+    if (!user_id) throw new Error(`user_id is a required paramter of the belongsTo function`)
+    let own = this.modelClass.queryFilters.own
+    if (!own) throw new Error(`model ${this._modelClass.name} does not have a own filter`)
+    own(this, user_id)
+    return this
+  }
+
+  applyFilters(filters) {
+    /*
+     * example filter
+     * 
+     * user_id: (query, user_id) => query.where({ user_id })
+     * 
+     */
+    if (!filters) return this
+    Object.keys(filters).forEach(filterName => {
+      let filterFunction = this._modelClass.queryFilters[filterName]
+      if (!filterFunction) throw new Error(`Unknown filter ${filterName} in model ${this.modelClass.name}`)
+      filterFunction(this, filters[filterName])
+    })
+    return this
+  }
+
+  eagerFromFields(fields, owner) {
+    
+    if (!fields) return this
+    let model = this._modelClass
+
+    const [expression, filters] = buildEager(model, fields, owner)
 
     return this.eager(expression, filters)
-  }
-
-  allowInsertFromGraph(graph, model) {
-
-  }
-
-  allowUpsertFromGraph(graph, model) {
-
   }
 }
 
@@ -28,44 +47,37 @@ class BaseModel extends Model {
 
 }
 
-function buildEager (fields, graph, model) {
-  let FILTER_INDEX = 0
-  const EAGERFILTERS = {}
+module.exports = function (config) {
 
-  return [expression, filters]
+  const {
+    tableName,
+    idColumn,
+    name,
+    description,
+    filters,
+    schema,
+    relations,
+  } = config
 
-  function buildEagerFields(fields, parentModel) {
-    let stringFields = []
-    let relations = parentModel.getRelations()
-
-    fields.forEach(field => {
-      let relation = relations[fieldName]
-      // if relation does not exist, ignore
-      // todo: separate natural fields and model relations in graph class
-      if (!relation) return
-      let fieldName = graph.root(field)
-      let filters = buildEagerFilters(graph.filters(field), relation.relatedModelClass)
-      let subFields = buildEagerFields(graph.fields(field), relation.relatedModelClass)
-      stringFields.push(`${fieldName}${filters}${subFields.length?'.'+ subFields : ''}`)
-    })
-
-    return stringFields.length ? `[${stringFields.join(', ')}]` : ''
-  }
- 
-  function buildEagerFilters (filters, model) {
-    let stringFilters = []
-    Object.keys(filters).forEach(filterName => {
-      let filterValue = filter[filterName]
-      let filterFunction = model.filters[filterName]
-      let filterKey = `f${FILTER_INDEX}_${model.name}_${filterName}_${filterValue}`
-      FILTER_INDEX += 1
-      // todo: use some default filters accross multiple models
-      if (!filterFunction) throw new Error(`Unknown filter ${filterName} on model ${model.name}`)
-      EAGERFILTERS[filterKey] = query => filterFunction(query, filterValue)
-      stringFilters.push(filterKey)
-    })
-    return stringFilters.length ? `(${stringFilters.join(', ')})` : ''
-  }
+  // todo: validate model
+  
+  return {
+    [name]: class extends BaseModel {
+      static get tableName() { return tableName }
+      static get idColumn() { return idColumn }
+      static get description() { return description }
+      static get resource() { return resource }
+      static get jsonSchema () {
+        return {
+          name,
+          description,
+          ...schema
+        }
+      }
+      static get relationMappings () {
+        return relations()
+      }
+      static get queryFilters() { return filters }
+    }
+  }[name]
 }
-
-module.exports = BaseModel
