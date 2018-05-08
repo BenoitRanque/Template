8c8a0fce39cc89197d2b84e25684b1472e4f0ca4
BenoitRanque <ranque.benoit@gmail.com>
2018-05-08 00:14:05 -0400

    added authorization getter

--- client/src/store/session/getters.js
+++ client/src/store/session/getters.js
@@ -8,10 +8,24 @@ export function isAuthenticated ({ user }) {
 }
 
 export function isAuthorized ({ user }, { isAuthenticated }) {
-  return (module, privNames) => {
-    return isAuthenticated() && user.roles.some(role => {
-      return role.module === module && role.privileges.some(privilege => {
-        return privNames.includes(privilege.name)
+  return (resource = [], action = ['read', 'create', 'update', 'delete'], possession = 'any') => {
+    // example privilege structure
+    // const privileges = {
+    //   resource: {
+    //     action: ['posession'],
+    //   }
+    // }
+    const { privileges } = user
+
+    const optionalArrayIteration = (values, method) => {
+      return Array.isArray(values) ? values.some(method) : method(values)
+    }
+
+    return isAuthenticated() && optionalArrayIteration(resource, resource => {
+      return privileges.hasOwnProperty(resource) && optionalArrayIteration(action, action => {
+        return privileges[resource].hasOwnProperty(action) && optionalArrayIteration(possession, possession => {
+          return privileges[resource][action].includes(possession)
+        })
       })
     })
   }
