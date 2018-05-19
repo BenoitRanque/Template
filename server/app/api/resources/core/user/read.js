205e97d5eb17b170d113838c55bc99a1d31cc9e3
BenoitRanque <ranque.benoit@gmail.com>
2018-05-18 23:40:38 -0400

    read user no longer gets password

--- server/app/api/resources/core/user/read.js
+++ server/app/api/resources/core/user/read.js
@@ -1,7 +1,7 @@
 module.exports = async (input, params, { authorize, model }) => {
   let permission = authorize(model.resourceName, 'read', 'any')
 
-  let data = await model.query().eager('[role]').returning(['username', 'displayname', 'descripcion', 'role'])
+  let data = await model.query().eager('[role]')
 
   return permission.filter(data)
 }
