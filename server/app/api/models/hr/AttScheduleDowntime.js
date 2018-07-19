a93d7fb9407523b74c13fb5e9396ed488d3379e3
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-07-19 16:29:26 -0400

    forgot to commit this

--- server/app/api/models/hr/AttScheduleDowntime.js
+++ server/app/api/models/hr/AttScheduleDowntime.js
@@ -2,11 +2,11 @@ const Model = require('@tools/model')
 const { HasOneRelation, BelongsToOneRelation, HasOneThroughRelation, HasManyRelation, ManyToManyRelation } = Model
 const { formatDate, formatTime, parseDate, parseTime, parseInterval } = require('@tools/dateUtils')
 
-module.exports = class HRAttTimetable extends Model {
-  static get tableName () { return 'hr_att_timetable' }
-  static get idColumn () { return 'timetable_id' }
+module.exports = class HRAttScheduleDowntime extends Model {
+  static get tableName () { return 'hr_att_schedule_break' }
+  static get idColumn () { return 'schedule_break_id' }
   static get relationMappings () {
-    const AttType = require('./AttType')
+    const AttTimetype = require('./AttTimetype')
     const AttSchedule = require('./AttSchedule')
     return {
       'schedule': {
@@ -17,34 +17,15 @@ module.exports = class HRAttTimetable extends Model {
           to: AttSchedule.tableName + '.schedule_id'
         }
       },
-      'type': {
+      'timetype': {
         relation: HasOneRelation,
-        modelClass: AttType,
+        modelClass: AttTimetype,
         join: {
-          from: this.tableName + '.type_id',
-          to: AttType.tableName + '.type_id'
+          from: this.tableName + '.timetype_id',
+          to: AttTimetype.tableName + '.timetype_id'
         }
       }
     }
   }
-  async $beforeInsert (ctx) {
-    this.created_at = new Date().toISOString();
-    this.start_time ? this.start_time = formatTime(this.start_time) : null
-    this.end_time ? this.end_time = formatTime(this.end_time) : null
-    this.duration ? this.duration = formatTime(this.duration) : null
-  }
-
-  async $beforeUpdate (ctx) {
-    this.updated_at = new Date().toISOString();
-    this.start_time ? this.start_time = formatTime(this.start_time) : null
-    this.end_time ? this.end_time = formatTime(this.end_time) : null
-    this.duration ? this.duration = formatTime(this.duration) : null
-  }
-  
-  async $afterGet (ctx) {
-    this.start_time ? this.start_time = parseTime(this.start_time) : null
-    this.end_time ? this.end_time = parseTime(this.end_time) : null
-    this.duration ? this.duration = parseInterval(this.duration) : null
-    return this
-  }
 }
+
