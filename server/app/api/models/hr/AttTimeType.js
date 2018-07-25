960aa13a9fd3443985494840abca8b3a20f8b53e
Bienvenido Benoit Ranque <desarrollo@guembe.ti>
2018-07-25 10:25:39 -0400

    updated seeds and models

--- server/app/api/models/hr/AttTimeType.js
+++ server/app/api/models/hr/AttTimeType.js
@@ -5,28 +5,4 @@ const { formatDate, formatTime, parseDate, parseTime, parseInterval } = require(
 module.exports = class HRAttTimeType extends Model {
   static get tableName () { return 'hr_att_timetype' }
   static get idColumn () { return 'timetype_id' }
-
-  async $beforeInsert (ctx) {
-    this.created_at = new Date().toISOString();
-    this.start_early_threshold ? this.start_early_threshold = formatTime(this.start_early_threshold) : null
-    this.start_late_threshold ? this.start_late_threshold = formatTime(this.start_late_threshold) : null
-    this.end_early_threshold ? this.end_early_threshold = formatTime(this.end_early_threshold) : null
-    this.end_late_threshold ? this.end_late_threshold = formatTime(this.end_late_threshold) : null
-  }
-  
-  async $beforeUpdate (ctx) {
-    this.updated_at = new Date().toISOString();
-    this.start_early_threshold ? this.start_early_threshold = formatTime(this.start_early_threshold) : null
-    this.start_late_threshold ? this.start_late_threshold = formatTime(this.start_late_threshold) : null
-    this.end_early_threshold ? this.end_early_threshold = formatTime(this.end_early_threshold) : null
-    this.end_late_threshold ? this.end_late_threshold = formatTime(this.end_late_threshold) : null
-  }
-  
-  async $afterGet (ctx) {
-    this.start_early_threshold ? this.start_early_threshold = parseTime(this.start_early_threshold) : null
-    this.start_late_threshold ? this.start_late_threshold = parseTime(this.start_late_threshold) : null
-    this.end_early_threshold ? this.end_early_threshold = parseTime(this.end_early_threshold) : null
-    this.end_late_threshold ? this.end_late_threshold = parseTime(this.end_late_threshold) : null
-    return this
-  }
 }
--- server/app/db/seeds/06_hr_att_timetype.js
+++ server/app/db/seeds/06_hr_att_timetype.js
@@ -5,19 +5,16 @@ exports.seed = async function(knex, Promise) {
   // Deletes ALL existing entries
   await AttTimeType.query().del()
 
-  let zeroDate = new Date()
-  zeroDate.setHours(0, 0, 0, 0)
-
   await AttTimeType.query().insert([
-    { timetype_id: ATT_TIMEOFF, timetype_name: 'Tiempo Libre', description: '', code: 'L', color: '#FFF', start_early_threshold: zeroDate, start_late_threshold: zeroDate, end_early_threshold: zeroDate, end_late_threshold: zeroDate  },
-    { timetype_id: ATT_WORK, timetype_name: 'Tiempo Laboral', description: '', code: 'T', color: '#FFF', start_early_threshold: zeroDate, start_late_threshold: zeroDate, end_early_threshold: zeroDate, end_late_threshold: zeroDate  },
-    { timetype_id: ATT_BREAK, timetype_name: 'Almuerzo', description: '', code: 'A', color: '#FFF', start_early_threshold: zeroDate, start_late_threshold: zeroDate, end_early_threshold: zeroDate, end_late_threshold: zeroDate  },
-    { timetype_id: ATT_VACATION, timetype_name: 'Vacacion', description: '', code: 'V', color: '#FFF', start_early_threshold: zeroDate, start_late_threshold: zeroDate, end_early_threshold: zeroDate, end_late_threshold: zeroDate  },
-    { timetype_id: ATT_HOLIDAY, timetype_name: 'Feriado', description: '', code: 'F', color: '#FFF', start_early_threshold: zeroDate, start_late_threshold: zeroDate, end_early_threshold: zeroDate, end_late_threshold: zeroDate  },
-    { timetype_id: ATT_EXTRA, timetype_name: 'Tiempo Extra', description: '', code: 'E', color: '#FFF', start_early_threshold: zeroDate, start_late_threshold: zeroDate, end_early_threshold: zeroDate, end_late_threshold: zeroDate  },
-    { timetype_id: ATT_LEAVE_PAID, timetype_name: 'Permiso sin descuento', description: '', code: 'PSD', color: '#FFF', start_early_threshold: zeroDate, start_late_threshold: zeroDate, end_early_threshold: zeroDate, end_late_threshold: zeroDate  },
-    { timetype_id: ATT_LEAVE_UNPAID, timetype_name: 'Permison con descuento', description: '', code: 'PCD', color: '#FFF', start_early_threshold: zeroDate, start_late_threshold: zeroDate, end_early_threshold: zeroDate, end_late_threshold: zeroDate  },
-    { timetype_id: ATT_LEAVE_SICK, timetype_name: 'Baja medica', description: '', code: 'BM', color: '#FFF', start_early_threshold: zeroDate, start_late_threshold: zeroDate, end_early_threshold: zeroDate, end_late_threshold: zeroDate  }
+    { timetype_id: ATT_TIMEOFF, timetype_name: 'Tiempo Libre', description: '', code: 'L', color: '#FFF' },
+    { timetype_id: ATT_WORK, timetype_name: 'Tiempo Laboral', description: '', code: 'T', color: '#FFF' },
+    { timetype_id: ATT_BREAK, timetype_name: 'Almuerzo', description: '', code: 'A', color: '#FFF' },
+    { timetype_id: ATT_VACATION, timetype_name: 'Vacacion', description: '', code: 'V', color: '#FFF' },
+    { timetype_id: ATT_HOLIDAY, timetype_name: 'Feriado', description: '', code: 'F', color: '#FFF' },
+    { timetype_id: ATT_EXTRA, timetype_name: 'Tiempo Extra', description: '', code: 'E', color: '#FFF' },
+    { timetype_id: ATT_LEAVE_PAID, timetype_name: 'Permiso sin descuento', description: '', code: 'PSD', color: '#FFF' },
+    { timetype_id: ATT_LEAVE_UNPAID, timetype_name: 'Permison con descuento', description: '', code: 'PCD', color: '#FFF' },
+    { timetype_id: ATT_LEAVE_SICK, timetype_name: 'Baja medica', description: '', code: 'BM', color: '#FFF' }
   ])
 }
 
