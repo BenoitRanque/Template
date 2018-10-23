5bc537d0fc06ff5b07b0f6125c73919590913530
BenoitRanque <ranque.benoit@gmail.com>
2018-10-23 02:13:56 -0400

    minor cleanup of query an mutation resolver files

--- server3/src/resolvers/Mutation.js
+++ server3/src/resolvers/Mutation.js
@@ -2,29 +2,11 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { APP_SECRET, BCRYPT_SALT_ROUNDS, getUserId } = require('../utils')
 
-// function post(parent, args, context, info) {
-//   const userId = getUserId(context)
-//   return context.db.mutation.createLink(
-//     {
-//       data: {
-//         url: args.url,
-//         description: args.description,
-//         postedBy: { connect: { id: userId } },
-//       },
-//     },
-//     info,
-//   )
-// }
-
 async function createUser(parent, args, ctx, info) {
   const password = await bcrypt.hash(args.data.password, BCRYPT_SALT_ROUNDS)
-  const user = await ctx.db.mutation.createUser({
+  return ctx.db.mutation.createUser({
     data: { ...args.data, password },
-  }, `{ id }`)
-
-  // const token = jwt.sign({ userId: user.id }, APP_SECRET)
-
-  return ctx.db.query.user({ where: { id: user.id }}, info)
+  }, info)
 }
 
 async function authenticate(parent, args, ctx, info) {
@@ -50,33 +32,25 @@ async function createException(parent, { data }, ctx, info) {
   const employeeId = data.employee.id
   const exceptionDates = data.slots.map(({ date }) => date)
 
-  console.log('UserId', userId)
-  console.log('EmployeeId', data)
-
-  const userIsSupervisor = await ctx.db.query.employees({
-    where: {
-      id: employeeId,
-      department: {
-        supervisors_some: {
-          id: userId
-        }
+  const userIsSupervisor = await ctx.db.$exists.employee({
+    id: employeeId,
+    department: {
+      supervisors_some: {
+        id: userId
       }
     }
-  }, `{ id }`)
-  console.log('Hello', userIsSupervisor)
-  if (!userIsSupervisor.length) throw new Error('User is not an authorized supervisor of employee')
+  })
+  if (!userIsSupervisor) throw new Error('User is not an authorized supervisor of employee')
 
-  const exceptionsWithDuplicateDates = await ctx.db.query.exceptions({
-    where: {
-      employee: {
-        id: employeeId
-      },
-      slots_some: {
-        date_in: exceptionDates
-      }
+  const exceptionsWithDuplicateDates = await ctx.db.$exists.exception({
+    employee: {
+      id: employeeId
+    },
+    slots_some: {
+      date_in: exceptionDates
     }
-  }, `{ id }`)
-  if (exceptionsWithDuplicateDates.length > 0) throw new Error('Conflict with other exceptions: dates already exist')
+  })
+  if (exceptionsWithDuplicateDates) throw new Error('Conflict with other exceptions: dates already exist')
 
   return ctx.db.mutation.createException({
     data: {
@@ -112,7 +86,6 @@ async function createException(parent, { data }, ctx, info) {
 
 
 module.exports = {
-  // post,
   createUser,
   authenticate,
   createException,
@@ -124,25 +97,3 @@ module.exports = {
   updateDepartment: (parent, args, ctx, info) => ctx.db.mutation.updateDepartment(args, info),
   deleteDepartment: (parent, args, ctx, info) => ctx.db.mutation.deleteDepartment(args, info)
 }
-
-// vote
-// async function vote(parent, args, context, info) {
-//   const userId = getUserId(context)
-//   const linkExists = await context.db.exists.Vote({
-//     user: { id: userId },
-//     link: { id: args.linkId },
-//   })
-//   if (linkExists) {
-//     throw new Error(`Already voted for link: ${args.linkId}`)
-//   }
-
-//   return context.db.mutation.createVote(
-//     {
-//       data: {
-//         user: { connect: { id: userId } },
-//         link: { connect: { id: args.linkId } },
-//       },
-//     },
-//     info,
-//   )
-// }
--- server3/src/resolvers/Query.js
+++ server3/src/resolvers/Query.js
@@ -1,34 +1,4 @@
-// async function feed(parent, args, context, info) {
-//   const where = args.filter
-//     ? {
-//         OR:  [
-//           { url_contains:  args.filter },
-//           { description_contains:  args.filter },
-//         ],
-//       }
-//     : {}
-
-//   const queriedLinkes = await context.db.query.links(
-//     { where, skip:  args.skip, first:  args.first, orderBy:  args.orderBy },
-//     `{ id }`,
-//   )
-
-//   const countSelectionSet = `
-//     {
-//       aggregate {
-//         count
-//       }
-//     }
-//   `
-//   const linksConnection = await context.db.query.linksConnection({}, countSelectionSet)
-
-//   return {
-//     count:  linksConnection.aggregate.count,
-//     linkIds:  queriedLinkes.map(link => link.id),
-//   }
-// }
 const { getUserId } = require('../utils')
-const loadAttendanceReport = require('../loadAttendanceReport')
 
 module.exports = {
   // node (parent, { id }, ctx, info) {
