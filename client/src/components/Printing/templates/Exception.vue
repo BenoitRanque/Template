b857365c827cecc0ce919e6a9f9f0d23278dd523
BenoitRanque <ranque.benoit@gmail.com>
2018-11-12 07:33:37 -0400

    more work on printing template

--- client/src/components/Printing/templates/Exception.vue
+++ client/src/components/Printing/templates/Exception.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="row items-center">
-      <div class="col q-title">
-        <div class="q-title">
+      <div class="col q-title q-my-sm">
+        <div class="q-headline">
           Boleta de Autorizacion
         </div>
         <!-- <div class="q-caption">Identificador Unico</div>
@@ -20,13 +20,33 @@
         <div style="font-family: monospace, monospace; font-size: 14px;">{{payload.id}}</div> -->
       </div>
       <div class="col-auto">
-        <div class="q-caption">Fecha de creacion: {{formatDate(payload.createdAt, 'DD/MM/YYYY')}}</div>
+        <div class="q-caption">Fecha: {{formatDate(payload.createdAt, 'DD/MM/YYYY')}}</div>
       </div>
     </div>
-
     <div class="row">
-      <div class="col">Nombre de interesado</div>
-      <div class="col">{{payload.employee.nameFull}}</div>
+      <div class="col-3">Nombre:</div>
+      <div class="col-9">{{payload.employee.nameFull}}</div>
+    </div>
+    <div class="q-my-sm">
+      <div class="q-title">Dias</div>
+      <div class="row" v-for="(slot, index) in payload.slots" :key="index">
+        <div class="col-4">{{formatDate(slot.date, 'DD/MM/YYYY')}}</div>
+        <div class="col-8">{{slot.schedule.description}}</div>
+      </div>
+    </div>
+    <div v-if="credits" class="q-my-sm">
+      <div class="q-title">Saldos a favor</div>
+      <div class="row" v-for="(amount, creditCategory) in credits" :key="creditCategory">
+        <div class="col-4">{{amount}}</div>
+        <div class="col-8">{{categoryDescription(creditCategory)}}</div>
+      </div>
+    </div>
+    <div v-if="debits" class="q-my-sm">
+      <div class="q-title">Saldos en contra</div>
+      <div class="row" v-for="(amount, debitCategory) in debits" :key="debitCategory">
+        <div class="col-4">{{amount}}</div>
+        <div class="col-8">{{categoryDescription(debitCategory)}}</div>
+      </div>
     </div>
     <div class="q-px-lg row gutter-x-md">
       <div class="col">
@@ -61,6 +81,7 @@
 
 <script>
 import Barcode from '@chenfengyuan/vue-barcode'
+import { mapGetters } from 'vuex'
 import { date } from 'quasar'
 
 export default {
@@ -68,6 +89,37 @@ export default {
   props: ['payload'],
   components: {
     Barcode
+  },
+  computed: {
+    ...mapGetters('schedule', {
+      categoryDescription: 'categoryDescription'
+    }),
+    credits () {
+      return [{ category: 'SCH_DAY_OFF' }].reduce((credits, credit) => {
+        if (credits[credit.category] === undefined) {
+          credits[credit.category] = 0
+        }
+        credits[credit.category] += 0.5
+        return credits
+      }, {})
+      // if (!this.payload || !this.payload.credits || !this.payload.credits.length) return null
+      // return this.payload.credits.reduce((credits, credit) => {
+      //   if (credits[credit.category] === undefined) {
+      //     credits[credit.category] = 0
+      //   }
+      //   credits[credit.category] += 0.5
+      // }, {})
+    },
+    debits () {
+      if (!this.payload || !this.payload.debits || !this.payload.debits.length) return null
+      return this.payload.debits.reduce((debits, debit) => {
+        if (debits[debit.category] === undefined) {
+          debits[debit.category] = 0
+        }
+        debits[debit.category] += 0.5
+        return debits
+      }, {})
+    }
   },
   methods: {
     formatDate: date.formatDate
